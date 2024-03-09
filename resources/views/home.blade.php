400b22fccd3a1d446469e77ba79687810ebce3a4
BrianBontempi <brian.bont54@gmail.com>
2024-03-09 13:00:22 +0100

    added data passed to the homepage

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,11 +1,18 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Laravel home</title>
 </head>
+
 <body>
     <h1>Hello world!</h1>
+    @foreach ($users as $user)
+        <h2>Name: {{ $user['name'] }}</h2>
+        <h2>Surname: {{ $user['surname'] }}</h2>
+    @endforeach
 </body>
-</html> 
+
+</html>
--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,14 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('home');
+    $data=[
+        'users'=>[
+                    ['name'=>'Pippo',
+                    'surname'=>'Franco'],
+                    ['name'=>'Ciccio',
+                    'surname'=>'Rossi'],
+                ]
+        ];
+
+    return view('home', $data);
 });
